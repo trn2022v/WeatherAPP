1fa83b146fb6d69e447c6b6cececfd886de3b003
trn2022v <valentintuyrin@gmail.com>
2022-06-27 17:14:05 +0300

    add adapter

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weatherapp.view.adapters.MainDailyListAdapter
+import com.example.weatherapp.view.adapters.MainHourlyListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initViews()
+
+        main_hourly_list.apply {
+            adapter = MainHourlyListAdapter()
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            setHasFixedSize(true)
+
+        }
+        main_daily_list.apply {
+            adapter = MainDailyListAdapter( )
+            layoutManager= LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            setHasFixedSize(true)
+        }
+
+
+    }
+
+    private fun initViews() {
         main_city_name.text = "Minsk"
         main_date_name.text = "21 april"
         main_weather_conditional_icon.setImageResource(R.drawable.ic_sun)
-        main_temp.text = "25"
+        main_temp.text = "25\u00b0"
         main_min_temp.text = " 17"
         main_max_temp.text = "27"
         main_weather_img.setImageResource(R.mipmap.union3x)
@@ -22,7 +43,6 @@ class MainActivity : AppCompatActivity() {
         main_wind_speed_mu_tv.text = "2 m/s"
         main_sunup_mu_tv.text = "6:30"
         main_sundown_mu_tv.text = "22:24"
-
 
     }
 }

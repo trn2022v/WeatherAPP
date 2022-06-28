e64e56dc623c0f50cbc06ebab742016e86800c17
trn2022v <valentintuyrin@gmail.com>
2022-06-28 15:15:35 +0300

    make a BaseAdapter and fix bug

--- app/src/main/java/com/example/weatherapp/view/adapters/MainDailyListAdapter.kt
+++ app/src/main/java/com/example/weatherapp/view/adapters/MainDailyListAdapter.kt
@@ -1,27 +1,66 @@
 package com.example.weatherapp.view.adapters
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.example.weatherapp.R
+import com.example.weatherapp.business.model.DailyWeatherModel
+import com.google.android.material.textview.MaterialTextView
 
-class MainDailyListAdapter : RecyclerView.Adapter<MainDailyListAdapter.DailyViewHolder>() {
+class MainDailyListAdapter : BaseAdapter<DailyWeatherModel>() {
+
+
+    /*
+    * тут будет стоять обработчик нажатий
+    * */
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_main_daily, parent, false)
         return DailyViewHolder(view)
     }
 
+    @SuppressLint("NonConstantResourceId")
+    inner class DailyViewHolder(view: View) : BaseViewHolder(view) {
 
-    override fun onBindViewHolder(holder: DailyViewHolder, position: Int) {
-    }
+        @BindView(R.id.item_daily_date_tv)
+        lateinit var date: MaterialTextView
 
+        @BindView(R.id.item_daily_pop_tv)
+        lateinit var popRate: MaterialTextView
 
-    override fun getItemCount() = 8
+        @BindView(R.id.item_daily_min_temp_tv)
+        lateinit var minTemp: MaterialTextView
 
+        @BindView(R.id.item_daily_max_temp_tv)
+        lateinit var maxTemp: MaterialTextView
 
-    inner class DailyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        @BindView(R.id.item_daily_weather_conditional_icon)
+        lateinit var icon: ImageView
+
+        init {
+            ButterKnife.bind(this, itemView)
+        }
+
+        override fun bindView(position: Int) {
+            date.text= " 25 saturday"
+            popRate.text ="100%"
+            minTemp.text = "23°"
+            maxTemp.text = "33°"
+            if (position==3){
+
+            }
+            icon.setImageResource(R.drawable.ic_sun)
+
+        }
 
     }
 }
--- app/src/main/java/com/example/weatherapp/view/adapters/MainHourlyListAdapter.kt
+++ app/src/main/java/com/example/weatherapp/view/adapters/MainHourlyListAdapter.kt
@@ -1,12 +1,20 @@
 package com.example.weatherapp.view.adapters
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.example.weatherapp.R
+import com.example.weatherapp.business.model.HourlyWeatherModel
+import com.google.android.material.textview.MaterialTextView
 
-class MainHourlyListAdapter : RecyclerView.Adapter<MainHourlyListAdapter.HourlyViewHolder>() {
+
+class MainHourlyListAdapter : BaseAdapter<HourlyWeatherModel>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HourlyViewHolder {
@@ -15,13 +23,39 @@ class MainHourlyListAdapter : RecyclerView.Adapter<MainHourlyListAdapter.HourlyV
         return HourlyViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: HourlyViewHolder, position: Int) {
+    @SuppressLint("NonConstantResourceId")
+    inner class HourlyViewHolder(view: View) : BaseViewHolder(view) {
 
-    }
 
-    override fun getItemCount() = 10
 
-    inner class HourlyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        @BindView(R.id.item_hourly_time_tv)
+        lateinit var time: MaterialTextView
+
+        @BindView(R.id.item_hourly_temp_tv)
+        lateinit var temperature: MaterialTextView
+
+        @BindView(R.id.item_hourly_pop_tv)
+        lateinit var popRate: MaterialTextView
+
+        @BindView(R.id.item_hourly_weather_condition_icon)
+        lateinit var icon: ImageView
+
+
+        init {
+            ButterKnife.bind(this, itemView)
+
+        }
+
+        override fun bindView(position: Int) {
+            time.text = "14:00"
+            temperature.text = "33°"
+            popRate.text =" 100%"
+            if (position==1){
+                time.setTextColor(ContextCompat.getColor(time.context, R.color.purple_700))
+            }
+                icon.setImageResource(R.drawable.ic_sun)
+
+        }
 
     }
 }
